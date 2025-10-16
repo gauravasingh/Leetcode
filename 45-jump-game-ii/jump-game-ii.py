63d1afd05574e777b2878e17f71f9a9702cb3497
gauravasingh <94314669+gauravasingh@users.noreply.github.com>
2025-10-15 21:19:18 -0400

    Time: 3 ms (97.19%) | Memory: 13.3 MB (47.49%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+       
+        n = len(nums)
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(n - 1):  # we don't need to jump from the last index
+            farthest = max(farthest, i + nums[i])
+
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps
+
+        
