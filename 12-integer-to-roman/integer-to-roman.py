15b4eca0bc2665d04ac56c67b9319070bbefa44b
gauravasingh <94314669+gauravasingh@users.noreply.github.com>
2025-10-17 10:15:24 -0400

    Time: 11 ms (36.79%) | Memory: 12.3 MB (98.80%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -5,22 +5,17 @@ class Solution(object):
         :rtype: str
         """
         
-        val = [
-            1000, 900, 500, 400,
-            100,  90,  50,  40,
-            10,    9,   5,   4,  1
-        ]
-        symbols = [
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV", "I"
-        ]
+        roman_parts = []
+        val_sym_pairs = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+    ]
 
-        roman = ""
-        for i in range(len(val)):
-            while num >= val[i]:
-                roman += symbols[i]
-                num -= val[i]
-        return roman
+        for val, sym in val_sym_pairs:
+            count, num = divmod(num, val)
+            roman_parts.append(sym * count)
+
+        return ''.join(roman_parts)
 
         
