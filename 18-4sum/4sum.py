fba0e463166acc215eb2b3968998c1c36f278476
gauravasingh <94314669+gauravasingh@users.noreply.github.com>
2025-10-20 19:43:37 -0400

    Time: 27 ms (94.81%) | Memory: 12.4 MB (59.70%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -7,16 +7,31 @@ class Solution(object):
         """
         nums.sort()
         n = len(nums)
-        result = []
+        res = []
 
         for i in range(n - 3):
-            # Skip duplicates for the first number
+            # Skip duplicates
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
+            # Early termination: smallest possible sum > target
+            if nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target:
+                break
+
+            # Skip if current number + 3 largest numbers < target
+            if nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target:
+                continue
+
             for j in range(i + 1, n - 2):
-                # Skip duplicates for the second number
+                # Skip duplicates
                 if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                # Early termination again
+                if nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target:
+                    break
+
+                if nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target:
                     continue
 
                 left, right = j + 1, n - 1
@@ -25,9 +40,9 @@ class Solution(object):
                     total = nums[i] + nums[j] + nums[left] + nums[right]
 
                     if total == target:
-                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
 
-                        # Skip duplicates for third and fourth numbers
+                        # Skip duplicates
                         while left < right and nums[left] == nums[left + 1]:
                             left += 1
                         while left < right and nums[right] == nums[right - 1]:
@@ -41,4 +56,4 @@ class Solution(object):
                     else:
                         right -= 1
 
-        return result
+        return res
