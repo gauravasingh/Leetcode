eabf5b5c10ddc8a8f75247841f7b5b6ecf2ef6ae
gauravasingh <94314669+gauravasingh@users.noreply.github.com>
2025-10-20 18:17:43 -0400

    Time: 0 ms (100.00%) | Memory: 12.7 MB (48.13%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        def backtrack(current, open_count, close_count):
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+        
+     
+            if open_count < n:
+                backtrack(current + '(', open_count + 1, close_count)
+        
+     
+            if close_count < open_count:
+                backtrack(current + ')', open_count, close_count + 1)
+
+        result = []
+        backtrack('', 0, 0)  # Initialize with an empty string and 0 open/close parentheses
+        return result
