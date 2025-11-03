51dc043c94e9c6d95acd06a4f6e23420af13e869
gauravasingh <94314669+gauravasingh@users.noreply.github.com>
2025-11-02 23:09:35 -0500

    Time: 118 ms (26.08%) | Memory: 38.6 MB (65.68%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -0,0 +1,51 @@
+class TrieNode(object):
+    def __init__(self):
+        self.children = {}   # Dictionary to hold child nodes
+        self.isEnd = False   # Flag to mark the end of a word
+
+class Trie(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: None
+        """
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isEnd = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.isEnd
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
