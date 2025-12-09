2802458260832e3f6866e0c5c813e83ad091d806
gauravasingh <94314669+gauravasingh@users.noreply.github.com>
2025-12-09 09:05:43 -0500

    Time: 11 ms (6.54%) | Memory: 17.8 MB (44.58%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        from collections import Counter
+
+        freq = Counter(nums)
+        max_rows = max(freq.values())
+
+        #  empty rows
+        result = [[] for _ in range(max_rows)]
+
+        # Distribute each no in diff rows
+        for num, count in freq.items():
+            for i in range(count):
+                result[i].append(num)
+
+        return result
